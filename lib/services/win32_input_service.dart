7c78dcb4c5095ca424d4f70f0778618945d58a63
Mateus Junior <juniormateusknd@gmail.com>
2023-06-11 18:56:10 -0300

    refactor: Introduce win32_input.dart

--- lib/services/win32_input_service.dart
+++ lib/services/win32_input_service.dart
@@ -3,95 +3,9 @@ import 'dart:math';
 
 import 'package:ffi/ffi.dart';
 import 'package:flutter/foundation.dart';
+import 'package:remotecontrol/services/win32_input.dart';
 import 'package:remotecontrol_lib/input/virtualkeys.dart';
 import 'package:win32/win32.dart';
-
-Future<int> mouseClick(MBWrapper key, {int interval = 20}) async {
-  final mouse = calloc<INPUT>();
-
-  mouse.ref.type = INPUT_MOUSE;
-  mouse.ref.mi.dwFlags = key.keyDown;
-  var result = SendInput(1, mouse, sizeOf<INPUT>());
-  if (result != TRUE) {
-    if (kDebugMode) {
-      print('Error: ${GetLastError()}');
-    }
-    return result;
-  }
-
-  await Future.delayed(Duration(milliseconds: interval));
-
-  mouse.ref.mi.dwFlags = key.keyUp;
-  result = SendInput(1, mouse, sizeOf<INPUT>());
-  if (result != TRUE) {
-    if (kDebugMode) {
-      print('Error: ${GetLastError()}');
-    }
-    return result;
-  }
-
-  free(mouse);
-  return result;
-}
-
-void testMouseclick() {
-  print('Sending a right-click mouse event.');
-  final mouse = calloc<INPUT>();
-  mouse.ref.type = INPUT_MOUSE;
-  mouse.ref.mi.dwFlags = MOUSEEVENTF_RIGHTDOWN;
-  var result = SendInput(1, mouse, sizeOf<INPUT>());
-  if (result != TRUE) print('Error: ${GetLastError()}');
-
-  Sleep(1000);
-  mouse.ref.mi.dwFlags = MOUSEEVENTF_RIGHTUP;
-  result = SendInput(1, mouse, sizeOf<INPUT>());
-  if (result != TRUE) print('Error: ${GetLastError()}');
-
-  free(mouse);
-}
-
-void testSendinput() {
-  print('Switching to Notepad and going to sleep for a second.');
-  ShellExecute(0, TEXT('open'), TEXT('notepad.exe'), nullptr, nullptr, SW_SHOW);
-  Sleep(1000);
-
-  print('Sending the "A" key and the Unicode character "€".');
-  final kbd = calloc<INPUT>();
-  kbd.ref.type = INPUT_KEYBOARD;
-  kbd.ref.ki.wVk = VK_A;
-  var result = SendInput(1, kbd, sizeOf<INPUT>());
-  if (result != TRUE) print('Error: ${GetLastError()}');
-
-  kbd.ref.ki.dwFlags = KEYEVENTF_KEYUP;
-  result = SendInput(1, kbd, sizeOf<INPUT>());
-  if (result != TRUE) print('Error: ${GetLastError()}');
-
-  kbd.ref.ki.wVk = 0;
-  kbd.ref.ki.wScan = 0x20AC; // euro sign
-  kbd.ref.ki.dwFlags = KEYEVENTF_UNICODE;
-  result = SendInput(1, kbd, sizeOf<INPUT>());
-  if (result != TRUE) print('Error: ${GetLastError()}');
-
-  kbd.ref.ki.dwFlags = KEYEVENTF_UNICODE | KEYEVENTF_KEYUP;
-  result = SendInput(1, kbd, sizeOf<INPUT>());
-  if (result != TRUE) print('Error: ${GetLastError()}');
-
-  free(kbd);
-
-  print('Sending a right-click mouse event.');
-  final mouse = calloc<INPUT>();
-  mouse.ref.type = INPUT_MOUSE;
-  mouse.ref.mi.dwFlags = MOUSEEVENTF_RIGHTDOWN;
-  result = SendInput(1, mouse, sizeOf<INPUT>());
-  if (result != TRUE) print('Error: ${GetLastError()}');
-
-  Sleep(1000);
-  mouse.ref.mi.dwFlags = MOUSEEVENTF_RIGHTUP;
-  result = SendInput(1, mouse, sizeOf<INPUT>());
-  if (result != TRUE) print('Error: ${GetLastError()}');
-
-  free(mouse);
-}
 
 class Win32InputService {
   Future<int> sendVirtualKey(int virtualKeyCode, {int interval = 20}) async {
